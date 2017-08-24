4043f99631d19f71b858d68888e5cca15d851a5b
Chris Deighton <chris@serato.com>
2017-08-24 21:40:32 +1200

    Create EventProcessor app

---
+++ apps/event_processor/lib/event_processor.ex
@@ -0,0 +1,18 @@
+defmodule EventProcessor do
+  @moduledoc """
+  Documentation for EventProcessor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EventProcessor.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
